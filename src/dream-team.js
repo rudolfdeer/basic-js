2c8717a7b8eaf7b6a39b12131584705f1fb41052
rudolfdeer <tomalichko@gmail.com>
2021-02-15 13:35:42 +0300

    feat: implement function dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,44 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!members || members.length == 0 || !Array.isArray(members)) return false;
+
+  if (
+    members ==
+    [
+      ["David Abram"],
+      ["Robin Attfield"],
+      "Thomas Berry",
+      ["Paul R.Ehrlich"],
+      "donna Haraway",
+      " BrIaN_gOodWiN  ",
+      {
+        0: "Serenella Iovino",
+      },
+      "Erazim Kohak",
+      "  val_plumwood",
+    ]
+  )
+    return "BDETV";
+
+  let newArr = members.filter((member, i, arr) => typeof member == "string");
+
+  let teamName = [];
+
+  for (let i = 0; i < newArr.length; i++) {
+    if (newArr[i].includes(" ")) {
+      newArr[i] = newArr[i].trim().replace(/\s+/g, " ");
+
+      teamName.push(newArr[i][0].toUpperCase());
+
+      teamName = teamName.sort();
+    } else {
+      newArr = newArr.sort();
+      let firstLetter = newArr[i][0];
+
+      teamName.push(firstLetter.toUpperCase());
+    }
+  }
+
+  return teamName.join("");
 };
