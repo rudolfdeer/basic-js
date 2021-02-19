67f567ce31affa550ce47d6f9b67138bf394176c
rudolfdeer <tomalichko@gmail.com>
2021-02-19 12:26:43 +0300

    feat: implement function vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,59 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
+  }
+
+  symmetric(sym) {
+    return sym.charCodeAt(0) - "A".charCodeAt(0);
+  }
+
+  shift(sym, length) {
+    const code = this.symmetric(sym);
+    return String.fromCharCode(((code + length) % 26) + "A".charCodeAt(0));
+  }
+
+  encrypt(string, key) {
+    return this.crypt(string, key, false);
+  }
+
+  decrypt(string, key) {
+    return this.crypt(string, key, true);
+  }
+
+  crypt(string, key, isDecrypt) {
+    if (string === undefined || key == undefined) {
+      throw new Error();
+    }
+
+    string = string.toUpperCase();
+    key = key.toUpperCase();
+
+    const res = [];
+    let coding = 0;
+    for (let i = 0; i < string.length; ++i) {
+      if (string[i] >= "A" && string[i] <= "Z") {
+        if (isDecrypt) {
+          res.push(
+            this.shift(string[i], 26 - this.symmetric(key[coding % key.length]))
+          );
+        } else {
+          res.push(
+            this.shift(string[i], this.symmetric(key[coding % key.length]))
+          );
+        }
+        coding++;
+      } else {
+        res.push(string[i]);
+      }
+    }
+
+    if (!this.direct) {
+      res.reverse();
+    }
+
+    return res.join("");
   }
 }
 
